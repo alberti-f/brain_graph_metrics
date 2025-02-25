fix mean communicability calculation

src/brain_graph_metrics.py
@@ -191,7 +191,8 @@ def compute_node_metrics(G, communities=None, no_com=False):
         communicability = dict(zip(G.nodes(), np.full(len(G), np.nan)))
     else:
         communicability_dict = nx.communicability(G)
-        communicability = {node: np.nanmean(comm.values()) for node, comm in communicability_dict.items()}
+        communicability = {node: np.nanmean([c for _, c in comm.items()])
+                           for node, comm in communicability_dict.items()}
 
     # Compute nodal efficiency (average inverse shortest path lengths)
     global_eff = global_efficiency_node(G)